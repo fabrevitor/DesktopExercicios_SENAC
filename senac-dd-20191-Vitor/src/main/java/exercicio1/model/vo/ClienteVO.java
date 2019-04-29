bba97334a229b6771bb67007105569988283a57c
Vitor <Vitor@Lenovo_Vitor>
2019-04-29 08:59:07 -0300

    Teste

--- senac-dd-20191-Vitor/src/main/java/exercicio1/model/vo/ClienteVO.java
+++ senac-dd-20191-Vitor/src/main/java/exercicio1/model/vo/ClienteVO.java
@@ -1,16 +1,16 @@
 package exercicio1.model.vo;
 
-public class ClienteVO{
+public class ClienteVO {
 	private int cpf;
 	private String nome;
 	private int idade;
 	private String[] telefone;
 	private EnderecoVO endereco;
-	
+
 	public ClienteVO() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
+
 	public ClienteVO(int cpf, String nome, int idade, String[] telefone, EnderecoVO endereco) {
 		super();
 		this.cpf = cpf;
@@ -19,33 +19,43 @@ public class ClienteVO{
 		this.telefone = telefone;
 		this.endereco = endereco;
 	}
+
 	public int getCpf() {
 		return cpf;
 	}
+
 	public void setCpf(int cpf) {
 		this.cpf = cpf;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public int getIdade() {
 		return idade;
 	}
+
 	public void setIdade(int idade) {
 		this.idade = idade;
 	}
+
 	public String[] getTelefone() {
 		return telefone;
 	}
+
 	public void setTelefone(String[] telefone) {
 		this.telefone = telefone;
 	}
+
 	public EnderecoVO getEndereco() {
 		return endereco;
 	}
+
 	public void setEndereco(EnderecoVO endereco) {
 		this.endereco = endereco;
 	}
